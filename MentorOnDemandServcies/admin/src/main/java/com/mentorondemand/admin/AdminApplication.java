af991764704aa94ce883b83d849da2a1e58f2cdb
prasenjitc29 <prasenjitc29@gmail.com>
2020-07-27 11:47:21 +0530

    services

--- MentorOnDemandServcies/admin/src/main/java/com/mentorondemand/admin/AdminApplication.java
+++ MentorOnDemandServcies/admin/src/main/java/com/mentorondemand/admin/AdminApplication.java
@@ -1,5 +1,7 @@
 package com.mentorondemand.admin;
 
+import java.util.Collections;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.client.RestTemplateBuilder;
@@ -9,10 +11,19 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 import org.springframework.web.client.RestTemplate;
 
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
 @SpringBootApplication
 //@EnableJpaRepositories
 //@EnableTransactionManagement
 @EnableDiscoveryClient
+@EnableSwagger2
 public class AdminApplication {
 
 	public static void main(String[] args) {
@@ -22,5 +33,25 @@ public class AdminApplication {
 	@Bean
 	public RestTemplate restTemplate(RestTemplateBuilder builder) {
 		return builder.build();
+	}
+	
+	@Bean
+	public Docket api() {                
+	    return new Docket(DocumentationType.SWAGGER_2)          
+	      .select()
+	      .apis(RequestHandlerSelectors.basePackage("com.mentorondemand.admin.controller"))
+//	      .paths(PathSelectors.ant("/api/*"))
+	      .build()
+	      .apiInfo(apiInfo());
+	}
+	 
+	private ApiInfo apiInfo() {
+	    return new ApiInfo(
+	      "Admin Service API", 
+	      "User & Auth", 
+	      "Mentor On demand", 
+	      "Terms of service", 
+	      new Contact("Prasenjit Choudhury", "http://localhost:4200", "prasenjitc29@gmail.com"), 
+	      "2020", "None", Collections.emptyList());
 	}
 }
--- MentorOnDemandServcies/search-service/src/main/java/com/lti/mod/searchservice/SearchServiceApplication.java
+++ MentorOnDemandServcies/search-service/src/main/java/com/lti/mod/searchservice/SearchServiceApplication.java
@@ -1,5 +1,7 @@
 package com.lti.mod.searchservice;
 
+import java.util.Collections;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
@@ -9,9 +11,17 @@ import io.searchbox.client.JestClient;
 import io.searchbox.client.JestClientFactory;
 import io.searchbox.client.config.ElasticsearchVersion;
 import io.searchbox.client.config.HttpClientConfig;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
 @EnableDiscoveryClient
+@EnableSwagger2
 public class SearchServiceApplication {
 
 	public static void main(String[] args) {
@@ -30,4 +40,26 @@ public class SearchServiceApplication {
 	    return factory.getObject();
 	}
 
+	@Bean
+	public Docket api() {                
+	    return new Docket(DocumentationType.SWAGGER_2)          
+	      .select()
+	      .apis(RequestHandlerSelectors.basePackage("com.lti.mod.searchservice.controller"))
+//	      .paths(PathSelectors.ant("/api/*"))
+//	      .apis(RequestHandlerSelectors.any())              
+//          .paths(PathSelectors.any())      
+	      .build()
+	      .apiInfo(apiInfo());
+	}
+	 
+	private ApiInfo apiInfo() {
+	    return new ApiInfo(
+	      "Search Service API", 
+	      "Search & Recs", 
+	      "Mentor On demand", 
+	      null, 
+//	      new Contact("Prasenjit Choudhury", "http://localhost:4200", "prasenjitc29@gmail.com"),
+	      null,
+	      "2020", "None", Collections.emptyList());
+	}
 }
